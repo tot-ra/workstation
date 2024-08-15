164fc631040189629c69ae36b40adc25c96a5701
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-08-15 18:07:10 +0300

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -2,3 +2,6 @@ echo "Installing nvm / node env"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 npm completion >> ~/.bash_profile
 npm completion >> ~/.zshrc
+
+nvm install 16
+nvm install 18
