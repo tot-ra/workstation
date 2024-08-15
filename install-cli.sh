3aab36c83e34de614da9e6f318bb875c281bab37
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-08-15 18:53:17 +0300

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -1,5 +1,12 @@
+echo "Install midnight commander file manager"
+brew install mc
+
 echo "Install yazi file manager"
-brew install yazi ffmpegthumbnailer sevenzip jq poppler fd ripgrep fzf imagemagick font-symbols-only-nerd-font
+brew install yazi ffmpegthumbnailer sevenzip jq poppler fd ripgrep imagemagick font-symbols-only-nerd-font
+
+echo "Install fzf fuzzy finder"
+brew install fzf
+source <(fzf --zsh)
 
 echo "Install eza a better ls"
 brew install eza
@@ -7,5 +14,11 @@ brew install eza
 echo "Install zoxide a better cd"
 brew install zoxide
 
-echo "Install bat, alternative to cat"
+echo "Install bat file previewer, alternative to cat"
 brew install bat
+
+echo "Install btop process manager, alternative to htop"
+brew install btop
+
+echo "Install json viewer"
+brew install jless
