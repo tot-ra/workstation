eb2050c0fba4321f241e0f46158f88c3fefac7a1
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-08-15 18:36:10 +0300

    Create install-cli.sh

---
+++ install-cli.sh
@@ -0,0 +1,11 @@
+echo "Install yazi file manager"
+brew install yazi ffmpegthumbnailer sevenzip jq poppler fd ripgrep fzf imagemagick font-symbols-only-nerd-font
+
+echo "Install eza a better ls"
+brew install eza
+
+echo "Install zoxide a better cd"
+brew install zoxide
+
+echo "Install bat, alternative to cat"
+brew install bat
