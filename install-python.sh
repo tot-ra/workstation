46e94e14e8eaf8631b639fba28242ff2763ec8fa
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-08-16 12:04:31 +0300

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -2,11 +2,14 @@
 brew install zlib openssl
 brew install freetype
 brew install pyenv
+
+brew install python@3.11
+brew link --force --overwrite python@3.11
+
 export PATH="$HOME/.pyenv/shims:$PATH"
 pyenv install --skip-existing 3.11
 pyenv global 3.11
 pyenv global
-
 
 pip install --upgrade pip
 pip install virtualenv
