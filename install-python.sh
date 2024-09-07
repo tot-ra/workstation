dde59e5908e0d412c2a66694ae784e1dfcc2f523
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-09-07 14:42:07 +0300

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -7,6 +7,8 @@ brew install python@3.11
 brew link --force --overwrite python@3.11
 
 export PATH="$HOME/.pyenv/shims:$PATH"
+pyenv install --skip-existing 3.7
+pyenv install --skip-existing 3.10
 pyenv install --skip-existing 3.11
 pyenv global 3.11
 pyenv global
