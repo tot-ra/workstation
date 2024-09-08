e55ea93ffea4156c40cbb6ba4e8a54c6ecf1cc0f
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-09-08 16:25:32 +0300

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -61,6 +61,11 @@ echo "Install neovim"
 brew install neovim
 git config --global core.editor nvim
 
+echo "Install lazyvim"
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
+
 echo "Install tmux"
 brew install tmux
 
