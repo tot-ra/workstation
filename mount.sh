04d6729b2cfc9e5c475f164132025054adc878e4
Artjom Kurapov <artkurapov@gmail.com>
2024-09-14 00:54:37 +0300

    automatic naming of tmux windows based on current path

--- mount.sh
+++ mount.sh
@@ -11,13 +11,13 @@ setopt hist_verify
 bindkey '^[[A' history-search-backward
 bindkey '^[[B' history-search-forward
 
-
 # dont warn about echoing on new terminal
 typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
 
-source ~/git/mac-work/aliases.sh
+source ~/git/mac-work/.aliases
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
