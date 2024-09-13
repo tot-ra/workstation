ad5a77f419d8d5ec0a04a1bda9876022d6bad37c
Artjom Kurapov <artkurapov@gmail.com>
2024-09-13 16:41:13 +0300

    add mysql client for nvim db panel

--- install.sh
+++ install.sh
@@ -37,5 +37,9 @@ sudo chmod 755 /usr/local/bin/dry
 echo "Installing redis"
 brew install redis
 
+echo "Installing mysql client for VIM to connect to DBs. Use 8.0 for native password support"
+brew install mysql-client@8.0
+brew link --overwrite mysql-client@8.0 --force
+
 ./install-node.sh
 ./install-python.sh
