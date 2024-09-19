e5bf92261a2c004ccc4bc74233e3bae749ce3197
Artjom Kurapov <artkurapov@gmail.com>
2024-09-19 13:37:59 +0300

    fix nvim startup

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -41,50 +41,7 @@ return {
     },
   },
   config = function(_, opts)
-    if opts.adapters then
-      local adapters = {}
-      for name, config in pairs(opts.adapters or {}) do
-        if type(name) == "number" then
-          if type(config) == "string" then
-            config = require(config)
-          end
-          adapters[#adapters + 1] = config
-        elseif config ~= false then
-          local adapter = require(name)
-          if type(config) == "table" and not vim.tbl_isempty(config) then
-            local meta = getmetatable(adapter)
-            if adapter.setup then
-              adapter.setup(config)
-            elseif adapter.adapter then
-              adapter.adapter(config)
-              adapter = adapter.adapter
-            elseif meta and meta.__call then
-              adapter(config)
-            else
-              error("Adapter " .. name .. " does not support setup")
-            end
-          end
-          adapters[#adapters + 1] = adapter
-        end
-      end
-      opts.adapters = adapters
-    end
-
-    -- Set up Neotest.
     require("neotest").setup(opts)
-
-    -- enable logging
-    local log = false
-    if log == true then
-      local filepath = require("neotest.logging"):get_filename()
-      vim.notify("Erasing Neotest log file: " .. filepath, vim.log.levels.WARN)
-      vim.fn.writefile({ "" }, filepath)
-
-      -- Enable during Neotest adapter development only.
-      local log_level = vim.log.levels.DEBUG
-      vim.notify("Logging for Neotest enabled", vim.log.levels.WARN)
-      require("neotest.logging"):set_level(log_level)
-    end
   end,
   keys = {
     { "<leader>t", mode = { "n", "v" }, "", desc = "+test" },
