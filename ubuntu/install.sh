a133d8ab363d63b701790ee72ebbf06fea10640d
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-09-22 04:06:16 +0300

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -16,23 +16,6 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 echo "Installing go"
 sudo apt install golang-go -y
 
-## CLI
-echo "Installing zsh"
-sudo apt install zsh
-
-echo "setting zsh to be default"
-chsh -s /usr/bin/zsh
-zsh --version
-
-echo "Installing eza, an alternative to ls"
-sudo apt install -y gpg
-sudo mkdir -p /etc/apt/keyrings
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-sudo apt update
-sudo apt install -y eza
-
 
 ## Detect if an Nvidia card is attached, and install the graphics drivers automatically if not already installed
 if [[ -n $(lspci | grep -i nvidia) && ! $(command -v nvidia-smi) ]]; then
@@ -51,23 +34,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.zshrc
 nvm install 20
 
-
-echo "Installing bat"
-sudo apt install bat -y
-
-
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "Restarting zsh"
-exec zsh
-sudo chsh -s "$(command -v zsh)" "${USER}"
-
-echo "Installing powerlevel10k zsh theme"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-echo "ZSH_THEME='powerlevel10k/powerlevel10k'" >> ~/.zshrc
-
-echo "Install zsh-autosuggestions"
-sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" | sed '1,/plugins=/ s/.*/&\n\n/' >> ~/.zshrc
+./install-go.sh
+./install-nvim-tmux.sh
+./install-docker-k8s.sh
+./install-cli.sh
