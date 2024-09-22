52bf48278378e054c998242511f0505b3007d880
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-09-22 04:00:08 +0300

    Update install-term.sh

--- ubuntu/install-term.sh
+++ ubuntu/install-term.sh
@@ -5,6 +5,12 @@ wget ${font_url}
 unzip ${font_name} -d ~/.fonts
 fc-cache -fv
 
+echo "Installing wezterm"
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo apt update
+sudo apt install wezterm
+
 
 echo "Installing tmux"
 sudo apt install tmux -y
