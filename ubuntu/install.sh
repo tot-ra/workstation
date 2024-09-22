ed974f2fa599097a745c4698985574a8085ef726
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-09-22 03:38:51 +0300

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -14,10 +14,17 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 
 # Golang
 echo "Installing go"
-sudo apt install golang-go
-
+sudo apt install golang-go -y
 
 ## Docker and kubernetes
+echo "Install docker"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 
 echo "Install docker-compose"
