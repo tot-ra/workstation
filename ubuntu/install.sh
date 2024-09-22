0c5a62ac9ec1c1c69e404bcfbc362c02f1de4b5f
Artjom Kurapov <artjom.kurapov@clarifai.com>
2024-09-22 04:01:48 +0300

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -16,33 +16,6 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 echo "Installing go"
 sudo apt install golang-go -y
 
-## Docker and kubernetes
-echo "Install docker"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo systemctl status docker
-sudo usermod -aG docker ${USER}
-
-echo "Install docker-compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-echo "Install kubectl"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl version --client
-
-echo "Install k9s"
-go install github.com/derailed/k9s@latest
-
-echo "Installing dry"
-curl -sSf https://moncho.github.io/dry/dryup.sh | sudo sh
-sudo chmod 755 /usr/local/bin/dry
-
 ## CLI
 echo "Installing zsh"
 sudo apt install zsh
