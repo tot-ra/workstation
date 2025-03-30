393e8d01ff9ff031e7ad6f134fa469df8d3b1753
Artjom Kurapov <artkurapov@gmail.com>
2025-03-30 23:47:25 +0300

    update go version

--- install-go.sh
+++ install-go.sh
@@ -1,5 +1,6 @@
 echo "Installing go 1.22"
 brew install go@1.22
+brew link --force --overwrite go@1.22
 
 echo "Installing delve to debug go code"
 go install github.com/go-delve/delve/cmd/dlv@latest
