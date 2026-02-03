db984ac50c4a536307ec46c781d42de8e1ecef8e
Artjom Kurapov <artkurapov@gmail.com>
2026-02-03 21:54:15 +0200

    fix(nvim): disable mini.indentscope causing crashes on ARM64

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "zbirenbaum/copilot-cmp", enabled = false },
   { "giuxtaposition/blink-cmp-copilot", enabled = false },
   { "epwalsh/obsidian.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
 }
