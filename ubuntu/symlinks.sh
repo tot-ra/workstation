c6b12559e81f9177c8915c7b9e9ce8dd0f76c2ea
Artjom Kurapov <artkurapov@gmail.com>
2026-02-04 16:24:13 +0200

    refactor: move opencode config to .config/opencode and update symlinks

--- ubuntu/symlinks.sh
+++ ubuntu/symlinks.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 echo "Creating symlinks for configuration files"
 
-# Tmux config
+# Tmux config (symlink)
 rm -rf ~/.tmux.conf
-cp -f ~/git/workstation/.tmux.conf ~/.tmux.conf
+ln -s ~/git/workstation/.tmux.conf ~/.tmux.conf
 
 # Nvim config (symlink)
 rm -rf ~/.config/nvim
 ln -s ~/git/workstation/.config/nvim ~/.config/nvim
+
+# Opencode config (symlink)
+rm -rf ~/.config/opencode
+ln -s ~/git/workstation/.config/opencode ~/.config/opencode
 
 # Google Calendar MCP credentials (symlink)
 ln -sf ~/git/mind/google_calendar_client_secret.json ~/git/workstation/google_calendar_client_secret.json
