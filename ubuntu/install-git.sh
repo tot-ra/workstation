02f6c40254610b8047083e78c649826f3651818e
Artjom Kurapov <artkurapov@gmail.com>
2026-02-05 11:08:15 +0200

    fix lazygit

--- ubuntu/install-git.sh
+++ ubuntu/install-git.sh
@@ -10,16 +10,42 @@
   && sudo apt install gh -y
 
 # Install lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-cd /tmp
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-cd -
-
-# in jetson nano you can use:
-# cd ~/git/
-# git clone https://github.com/jesseduffield/lazygit.git
-# cd lazygit
-# go build
-# sudo cp lazygit /usr/local/bin/lazygit
+if ! command -v lazygit &> /dev/null; then
+  echo "Installing lazygit..."
+  
+  # Detect architecture
+  ARCH=$(uname -m)
+  case $ARCH in
+    x86_64)
+      LAZYGIT_ARCH="Linux_x86_64"
+      ;;
+    aarch64|arm64)
+      LAZYGIT_ARCH="Linux_arm64"
+      ;;
+    armv7l)
+      LAZYGIT_ARCH="Linux_armv7"
+      ;;
+    *)
+      echo "Error: Unsupported architecture: $ARCH"
+      exit 1
+      ;;
+  esac
+  
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  
+  if [ -z "$LAZYGIT_VERSION" ]; then
+    echo "Error: Could not fetch lazygit version"
+    exit 1
+  fi
+  
+  echo "Downloading lazygit v${LAZYGIT_VERSION} for ${LAZYGIT_ARCH}..."
+  cd /tmp || exit 1
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_${LAZYGIT_ARCH}.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit /usr/local/bin
+  rm -f lazygit.tar.gz lazygit
+  cd - > /dev/null
+  echo "lazygit installed successfully"
+else
+  echo "lazygit is already installed: $(lazygit --version | head -1)"
+fi
