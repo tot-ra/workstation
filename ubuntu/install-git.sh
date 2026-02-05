f0710b001e0785c68255dc6653564b2c671af066
Artjom Kurapov <artkurapov@gmail.com>
2026-02-05 10:29:15 +0200

    fix(ubuntu): install lazygit in temp dir and add git folder permissions
    - Change lazygit installation to use /tmp instead of current directory
    - Add opencode permission config to allow access to ~/git/**

--- ubuntu/install-git.sh
+++ ubuntu/install-git.sh
@@ -11,9 +11,11 @@
 
 # Install lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+cd /tmp
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+cd -
 
 # in jetson nano you can use:
 # cd ~/git/
