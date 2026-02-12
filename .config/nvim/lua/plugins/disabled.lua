2cf7f57bac22ce491a24bc0d47d997609055d977
Artjom Kurapov <artkurapov@gmail.com>
2026-02-12 21:48:53 +0200

    ...

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,7 +3,7 @@ return {
   { "zbirenbaum/copilot-cmp", enabled = false },
   { "giuxtaposition/blink-cmp-copilot", enabled = false },
   { "epwalsh/obsidian.nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
+  { "nvim-mini/mini.indentscope", enabled = false },
   -- Disabled due to crashes with nvim 0.12.0-dev (noice/nvim-notify API mismatch)
   { "folke/noice.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
